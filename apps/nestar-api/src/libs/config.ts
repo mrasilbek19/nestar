497f0a699b67145f794ac338d536860a7935f2d0
mrasilbek19 <asilbek190303@gmail.com>
2026-08-25 18:13:53 +0900

    feat: develop getAgents GraphQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,6 +1,8 @@
 import { ObjectId } from "bson";
 
 
+export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberVievs', 'memberRank']
+
 export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === "string" ? new ObjectId(target) : target;
 };
