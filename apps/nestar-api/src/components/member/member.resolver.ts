637f749e0f645cca0c45e6fdca755f782e352811
mrasilbek19 <asilbek190303@gmail.com>
2026-08-25 17:38:30 +0900

    feat: view system integration

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -11,6 +11,8 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
+
 
 @Resolver()
 export class MemberResolver {
@@ -57,12 +59,13 @@ export class MemberResolver {
     }
 
 
-
+    @UseGuards(WithoutGuard)
     @Query(() => Member)
-    public async getMember(@Args('memberId') input: string): Promise<Member> {
+    public async getMember(@Args('memberId') input: string,
+        @AuthMember('_id') memberId: ObjectId): Promise<Member> {
         console.log('Query: getMember');
         const targetId = shapeIntoMongoObjectId(input)
-        return this.memberService.getMember(targetId);
+        return this.memberService.getMember(memberId, targetId);
     }
 
 
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -8,11 +8,15 @@ import { MemberStatus } from '../../libs/enums/member.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { ObjectId } from 'mongoose';
+import { T } from '../../libs/types/common';
+import { ViewService } from '../view/view.service';
+import { ViewGroup } from '../../libs/enums/view.enum';
 
 @Injectable()
 export class MemberService {
     constructor(@InjectModel("Member") private readonly memberModel: Model<Member>,
-        private readonly authService: AuthService) { }
+        private authService: AuthService,
+        private viewService: ViewService) { }
 
     public async signup(input: MemberInput): Promise<Member> {
         input.memberPassword = await this.authService.hashPassword(input.memberPassword);
@@ -71,7 +75,7 @@ export class MemberService {
         return result;
     }
 
-    public async getMember(targetId: ObjectId): Promise<Member> {
+    public async getMember(memberId: ObjectId, targetId: ObjectId): Promise<Member> {
         const search: T = {
             _id: targetId,
             memberStatus: {
@@ -79,10 +83,19 @@ export class MemberService {
             },
         };
 
-        const targetMember = await this.memberModel.findOne(search).exec();
+        const targetMember = await this.memberModel.findOne(search).lean().exec();
 
         if (!targetMember) {
             throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+        }
+
+        if (memberId) {
+            const viewInput = { memberId: memberId, viewRefId: targetId, viewGroup: ViewGroup.MEMBER };
+            const newView = await this.viewService.recordView(viewInput);
+            if (newView) {
+                await this.memberModel.findOneAndUpdate(search, { $inc: { memberViews: 1 } }, { new: true }).exec();
+                targetMember.memberViews++;
+            }
         }
 
         return targetMember;
