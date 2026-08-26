e60438a6b016e0475e47b196c1b664f3290c1002
mrasilbek19 <asilbek190303@gmail.com>
2026-08-26 23:35:01 +0900

    feat: develop uploader logic for project

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -3,6 +3,18 @@ import { ObjectId } from "bson";
 
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberVievs', 'memberRank']
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberVievs']
+
+/** IMAGE CONFIGURATION **/
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+    const ext = path.parse(filename).ext;
+    return uuidv4() + ext;
+};
+
+
 export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === "string" ? new ObjectId(target) : target;
 };
