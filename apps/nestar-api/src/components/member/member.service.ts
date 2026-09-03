c57c26d17469c277daeebd2566d7db162d70b1c5
mrasilbek19 <asilbek190303@gmail.com>
2026-09-03 18:39:06 +0900

    fix:modify getMemebr develop meFollowed integration

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -14,10 +14,13 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { LikeService } from '../like/like.service';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
+import { Follower, Following, MeFollowed } from '../../libs/dto/follow/follow';
 
 @Injectable()
 export class MemberService {
-    constructor(@InjectModel("Member") private readonly memberModel: Model<Member>,
+    constructor(
+        @InjectModel("Member") private readonly memberModel: Model<Member>,
+        @InjectModel("Follow") private readonly followModel: Model<Follower | Following>,
         private authService: AuthService,
         private viewService: ViewService,
         private likeService: LikeService,
@@ -101,16 +104,11 @@ export class MemberService {
                 await this.memberModel.findOneAndUpdate(search, { $inc: { memberViews: 1 } }, { new: true }).exec();
                 targetMember.memberViews++;
             }
-            // meLiked
-            const likeInput = {
-                memberId: memberId,
-                likeRefId: targetId,
-                likeGroup: LikeGroup.MEMBER,
-            };
 
+            const likeInput = { memberId: memberId, likeRefId: targetId, likeGroup: LikeGroup.MEMBER };
             targetMember.meLiked = await this.likeService.checkLikeExistence(likeInput);
 
-            // meFollowed
+            targetMember.meFollowed = await this.checkSubscription(memberId, targetId);
         }
 
         return targetMember;
@@ -221,6 +219,15 @@ export class MemberService {
         if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 
         return result;
+    }
+
+    private async checkSubscription(
+        followerId: ObjectId,
+        followingId: ObjectId,
+    ): Promise<MeFollowed[]> {
+        const result = await this.followModel
+            .findOne({ followingId: followingId, followerId: followerId }).exec();
+        return result ? [{ followerId: followerId, followingId: followingId, myFollowing: true }] : [];
     }
 
     public async memberStatsEditor(
