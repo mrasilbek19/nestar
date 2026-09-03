2673f567493ffa3fd97dcdbe297c72b7ada48e5c
mrasilbek19 <asilbek190303@gmail.com>
2026-09-03 14:12:04 +0900

    feat: develop likeTargetMember graphQl API

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
-import { LikeResolver } from './like.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import LikeSchema from '../../schemas/Like.model';
+
 
 @Module({
-  providers: [LikeService, LikeResolver]
+  imports: [MongooseModule.forFeature([
+    {
+      name: 'Like',
+      schema: LikeSchema,
+    },
+  ])],
+  providers: [LikeService],
+  exports: [LikeService],
 })
-export class LikeModule {}
+export class LikeModule { }
